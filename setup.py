1ed1e082f49ff58bf30899e1052b8e010814e8fe
ningdynasty <58092194+ningdynasty@users.noreply.github.com>
2020-12-10 15:52:29 -0500

    Update setup.py to new repository location

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='push2-python',
       version='0.4',
       description='Utils to interface with Ableton\'s Push 2 from Python',
-      url='https://github.com/ffont/push2-python',
+      url='https://github.com/ningdynasty/push2-python',
       author='Frederic Font',
       author_email='frederic.font@gmail.com',
       license='MIT',
